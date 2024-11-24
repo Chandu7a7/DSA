ed22719bb5db7bd4c5b5eb6580ecf747902d70ae
chandu <chandugadeshwer@gmail.com>
2024-11-24 22:55:16 +0530

    soring built

---
+++ Sorting/Builtsort.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Builtsort {
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 3, 1, 2}; // Define the array
+        Arrays.sort(arr); // Sort the array in ascending order
+
+        // Print the sorted array
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
