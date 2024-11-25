0225f8c9e403e5b46c55cb4ea7c0b26e559ae57c
chandu <chandugadeshwer@gmail.com>
2024-11-25 23:59:20 +0530

    up

---
+++ 2DArray/multiarray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class multiarray {
+    public static void main(String[] args) {
+        int  arr[]={1,2,3,4,8,2,};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
